f4d46f91da7f40ac03adeaa84f69f07fc44a70d3
maqubela <61004304+maqubela@users.noreply.github.com>
2020-02-19 11:30:51 +0200

    Add files via upload

---
+++ cupcakes.sql
@@ -0,0 +1,8 @@
+type,price,quantity,size,barcode,id
+"Double chocolate",R10,20,25g,00112233,1
+"Vanilla ",R9,20,25g,01234564,2
+Rasberry,R10,20,25g,07789921,3
+"Cookies 'n cream",R12,20,25g,01254820,4
+"Mint Oreo",R15,20,25g,00112255,5
+Blueberry,R13,20,25g,02258826,6
+"Red velvet",R10,20,25g,01122555,7
