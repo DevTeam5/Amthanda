24604cb8869903f26cf2bd0278e351a1f439ee31
maqubela <61004304+maqubela@users.noreply.github.com>
2020-02-19 11:17:29 +0200

    Add files via upload

---
+++ chocolate.sql
@@ -0,0 +1,8 @@
+type,price,quantity,size,barcode,id
+Flake,R11,30,80g,02136548,1
+Tex,R9,25,78g,00558866,2
+KitKat,R10,20,70g,00446688,3
+Crunchie,R12,35,60g,00885566,4
+Aero,R15,15,50g,00668877,5
+"Dairy milk",R16,15,150g,05588021,6
+"Milky bar",R14,20,150g,02153468,7
