84e39c1396f7b356d39df31eb1e75b4f592ac356
maqubela <61004304+maqubela@users.noreply.github.com>
2020-02-19 12:10:08 +0200

    Add files via upload

---
+++ pies.sql
@@ -0,0 +1,8 @@
+type,price,quantity,size,barcode,id
+"Chicken ",R25,30,450g,00776452,1
+"Pepper steak",R25,30,450g,01155895,2
+"Steak & Kidney ",R25,30,450g,04585654,3
+"Curried mutton",R25,30,450g,05128965,4
+"Meat & potato",R25,30,450g,03214567,5
+"Beef & onion",R25,30,450g,04458866,6
+"Greek spinach ",R25,30,450g,07788855,7
