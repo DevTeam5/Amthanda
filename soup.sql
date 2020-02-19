bb53be04cb856916d498f8c82f63d3ffd9a54cdb
maqubela <61004304+maqubela@users.noreply.github.com>
2020-02-19 12:11:02 +0200

    Add files via upload

---
+++ soup.sql
@@ -0,0 +1,7 @@
+type,price,quantity,size,barcode,id
+Chicken,R15,5,250ml,01010222,1
+Vegatable,R14,5,250ml,03325140,2
+Noodles,R16,5,300ml,01100001,3
+tomatos,R13,5,250ml,00158796,4
+Pumpkin,R10,5,200ml,00788221,5
+Beef,R16,5,300ml,00225852,6
