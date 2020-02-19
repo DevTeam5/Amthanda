3cf90242f5997286ac454c8bbd78956760a9e50f
maqubela <61004304+maqubela@users.noreply.github.com>
2020-02-19 12:20:00 +0200

    Add files via upload

---
+++ veg.sql
@@ -0,0 +1,8 @@
+type,price,quantity,size,barcode,id
+Potato,R6,10,1kg,00124555,1
+"Onion ",R5,15,1kg,00445566,2
+Carrot,R10,15,1kg,02145682,3
+Tomato,R6,10,1kg,01122885,4
+Gabbage,R7,12,3kg,08822551,5
+"Corn ",R9,10,500g,01100110,6
+Butternut,R10,20,2kg,01254895,7
