0928514f893b9a837cea58068b18f19ca1660f37
maqubela <61004304+maqubela@users.noreply.github.com>
2020-02-19 12:05:47 +0200

    Add files via upload

---
+++ icecream.sql
@@ -0,0 +1,8 @@
+type,price,quantity,size,barcode,id
+"bubble gum",R8,10,"2/3 cup",00122545,1
+"Cherry ",R9,10,"2/3 cup",01234567,2
+Vanilla,R6,10,"2/3 cup",02335644,3
+Strawberry,R7,10,"2/3 cup",00123548,4
+Oreo,10,10,"2/3 cup",01125681,5
+"Choc mint",R11,10,"2/3 cup",01223545,6
+Banana,R6,10,"2/3 cup",01223652,7
