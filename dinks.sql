f478e1e482a7479dbbe502f778eb9ec3e8ff42ab
maqubela <61004304+maqubela@users.noreply.github.com>
2020-02-19 11:52:51 +0200

    Add files via upload

---
+++ dinks.sql
@@ -0,0 +1,8 @@
+type,price,quantity,size,barcode,id
+"Coca-cola ",15,12,1L,11005522,1
+Pepsi,15,12,1L,00447788,2
+Fanta,14,12,1L,01324587,3
+Sprite,16,12,1L,00448855,4
+Stoney,13,12,1L,11003355,5
+"Lemon twist",13,12,1L,00885522,6
+Appletiser,15,12,1L,01234567,7
