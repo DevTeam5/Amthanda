41666d9f7bab1b35fa75a9bc48b090fad1f85457
maqubela <61004304+maqubela@users.noreply.github.com>
2020-02-19 12:07:01 +0200

    Add files via upload

---
+++ milkshake.sql
@@ -0,0 +1,9 @@
+type,price,quantity,size,barcode,id
+Banana,R20,15,450ml,07756842,1
+Strawberry,R22,15,450ml,00221144,2
+Vanilla,R19,15,450ml,05469871,3
+Blueberry,R21,15,450ml,01144555,4
+Chocolate,R20,15,450ml,00114422,5
+"Choc mint",R25,15,450ml,00115545,6
+Rasberry,R22,15,450ml,01222548,7
+,,,NULL,NULL,8
